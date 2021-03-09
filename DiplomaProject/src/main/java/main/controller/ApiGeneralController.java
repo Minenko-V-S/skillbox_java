f88a9f1186f073b7189c3ab72d8a9c2c3021607e
VitaliyM <vitalij-minenko@yandex.ru>
2021-03-10 00:42:24 +0500

    /api/settings

--- DiplomaProject/src/main/java/main/controller/ApiGeneralController.java
+++ DiplomaProject/src/main/java/main/controller/ApiGeneralController.java
@@ -1,6 +1,7 @@
 package main.controller;
 
 import main.api.response.InitResponse;
+import main.api.response.SettingsResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class ApiGeneralController {
 
     public ApiGeneralController(InitResponse initResponse) {
         this.initResponse = initResponse;
+    }
+
+    @GetMapping("/api/settings")
+    private SettingsResponse settings(){
+        return new SettingsResponse();
     }
 
     @GetMapping("/api/init")
