26dbdbdadb65b9e9cb889342f0104cb0b876c403
VitaliyM <vitalij-minenko@yandex.ru>
2021-03-10 00:25:03 +0500

    /api/init

--- DiplomaProject/src/main/java/main/controller/DefaultController.java
+++ DiplomaProject/src/main/java/main/controller/DefaultController.java
@@ -3,6 +3,7 @@ package main.controller;
 //ToDo
 // для обычных запросов не через API (главная страница - /, в частности)
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DefaultController {
+
+
     @RequestMapping("/")
     public String index(Model model){
         return "index";
