e38bbf8544d7378709b4de8baedcbd42843edc06
VitaliyM <vitalij-minenko@yandex.ru>
2021-03-10 01:00:36 +0500

    /api/settings

--- DiplomaProject/src/main/java/main/controller/ApiGeneralController.java
+++ DiplomaProject/src/main/java/main/controller/ApiGeneralController.java
@@ -2,6 +2,9 @@ package main.controller;
 
 import main.api.response.InitResponse;
 import main.api.response.SettingsResponse;
+import main.services.SettingsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiGeneralController {
 
-    InitResponse initResponse;
+    private final InitResponse initResponse;
+    private final SettingsService settingsService;
 
-    public ApiGeneralController(InitResponse initResponse) {
+    public ApiGeneralController(InitResponse initResponse, SettingsService settingsService) {
         this.initResponse = initResponse;
+        this.settingsService = settingsService;
     }
 
     @GetMapping("/api/settings")
-    private SettingsResponse settings(){
-        return new SettingsResponse();
+    private ResponseEntity<SettingsResponse> settings(){
+        return new ResponseEntity<>(settingsService.getGlobalSettings(), HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping("/api/init")
